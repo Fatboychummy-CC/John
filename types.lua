39c0b041a9a2bbf572816cb868b91dd5feaddd08
Matthew W <fatboychummy@gmail.com>
2022-12-04 18:29:10 -0700

    Add `active` to specced_module

--- types.lua
+++ types.lua
@@ -29,4 +29,4 @@ function module.test(message) return 0 end
 ---@field identifier string The identifier of this module.
 ---@field description string The description of this module.
 
----@alias specced_module {spec:module_spec,module:module} A specced module.
+---@alias specced_module {spec:module_spec,module:module,active:boolean} A specced module.
